da5a4d46e19715bbeadd43b6afb1549d7ae512d4
Yitshhaq Fukushima <ymotse@gmail.com>
2019-12-13 21:11:33 -0200

    Init project.

---
+++ src/test/java/com/ymotse/BootstrapAuthJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ymotse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootstrapAuthJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
