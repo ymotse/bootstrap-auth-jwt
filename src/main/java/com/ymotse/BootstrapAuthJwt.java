e5404411eaf630b777e9ebdef8705079a9340094
Yitshhaq Fukushima <ymotse@gmail.com>
2019-12-13 21:31:23 -0200

    Configured initial settings.

---
+++ src/main/java/com/ymotse/BootstrapAuthJwt.java
@@ -0,0 +1,21 @@
+package com.ymotse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootVersion;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.SpringVersion;
+
+@SpringBootApplication
+public class BootstrapAuthJwt {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootstrapAuthJwt.class, args);
+		
+		System.out.println("\n\n\n#################################################################");
+		System.out.println("Bootstrap Auth JWT service started.");
+		System.out.println("Spring Version: " + SpringVersion.getVersion());
+		System.out.println("Spring Boot Version: " + SpringBootVersion.getVersion());
+		System.out.println("#################################################################\n\n\n");
+	}
+
+}
